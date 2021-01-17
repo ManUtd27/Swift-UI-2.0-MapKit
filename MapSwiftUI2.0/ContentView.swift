64d93b336ae56c5f25496654c2d9987d6fa6d8f8
Shawn Williams <jr.william09@gmail.com>
2021-01-17 09:08:55 -0600

    Initial Commit

---
+++ MapSwiftUI2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapSwiftUI2.0
+//
+//  Created by Shawn Williams on 1/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapSwiftUI2.0/MapSwiftUI2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  MapSwiftUI2_0App.swift
+//  MapSwiftUI2.0
+//
+//  Created by Shawn Williams on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MapSwiftUI2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
